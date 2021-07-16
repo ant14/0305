4cb055444d2cad22c3e679055e27b8399793f3f4
ant14 <ant14@1.231.80.185>
2021-07-16 11:09:34 +0900

    0305

---
+++ 0305/src/JavaAppExam.java
@@ -0,0 +1,8 @@
+
+public class JavaAppExam {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to JavaWorld!");
+		System.out.println("Java ¼ö¾÷");
+	}
+}
---
+++ 0305/src/Profile.java
@@ -0,0 +1,11 @@
+
+public class Profile {
+
+	public static void main(String[] args) {
+		System.out.println("서울로봇고등학교");
+		System.out.println("2학년2반16번");
+		System.out.println("정한음");
+		
+	}
+
+}
